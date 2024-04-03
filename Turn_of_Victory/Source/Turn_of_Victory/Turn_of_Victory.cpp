428994952b81e4048d291bad65915119c94d1b52
ZhuohaoHe <zhuohao.he@outlook.com>
2024-04-02 22:39:32 -0400

    Update Init

--- Turn_of_Victory/Source/Turn_of_Victory/Turn_of_Victory.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Turn_of_Victory.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Turn_of_Victory, "Turn_of_Victory" );
